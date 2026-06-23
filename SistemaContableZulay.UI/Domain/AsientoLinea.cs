1cc568d3c948224fe9e202df9f4099f94905ac46
Jesus Antonio Chacon Moncada <164936430+JChaconMoncada@users.noreply.github.com>
2026-06-23 14:15:09 -0400

    Revert "Integracion de metodos basicos, solo funcionan un par de cosas, aun n…"

--- SistemaContableZulay.UI/Domain/AsientoLinea.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaContableZulay.UI.Domain;
-
-public class AsientoLinea
-{
-    public string CodigoCuenta { get; set; } = string.Empty;
-    public string DescripcionCuenta { get; set; } = string.Empty;
-    public decimal Debe { get; set; }
-    public decimal Haber { get; set; }
-}
--- SistemaContableZulay.UI/Domain/Moneda.cs
+++
@@ -1,8 +0,0 @@
-namespace SistemaContableZulay.UI.Domain;
-
-public class Moneda
-{
-    public string Codigo { get; set; } = string.Empty;
-    public string Nombre { get; set; } = string.Empty;
-    public decimal TasaCambio { get; set; }
-}
